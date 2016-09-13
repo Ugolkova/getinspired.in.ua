e5a5931839aeb352d96cc25c4a62ff6f03b64736
ugolkova <ugolkova@localhost>
2016-09-13 11:14:29 +0300

    Subscription

--- wp-content/themes/getinspired/index.php
+++ wp-content/themes/getinspired/index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
                 <h1>Не трать время, ожидая вдохновение.</h1>
                 <h2>Начни. И вдохновение тебя найдет!</h2>
-                <?php get_sidebar(); ?>
+                
+<?php echo do_shortcode('[mc4wp_form id="28"]'); ?>
 <?php get_footer(); ?>
--- wp-content/themes/getinspired/js/custom.js
+++ wp-content/themes/getinspired/js/custom.js
@@ -5,4 +5,36 @@ $(document).ready(function(){
             scrollTop: $(el).offset().top}, 200);
         return false; 
     });
+
+    $('.mc4wp-form').find('input[type="email"]').focusin(function(){
+        $(this).closest('form').addClass('active');
+    });
+    $('.mc4wp-form').find('input[type="email"]').focusout(function(){
+        $(this).closest('form').removeClass('active');
+    });
+    
+    $('.mc4wp-form').find('input[type="submit"]').hover(function(){
+        $(this).closest('form').addClass('active');
+    }, function(){
+        $(this).closest('form').removeClass('active');
+    });
+        
+    $('.mc4wp-form').find('input[type="submit"]').click(function(){
+        var data = {};
+        var form = $(this).closest('form');
+        
+        data.EMAIL = $('form').find('input[name="EMAIL"]').val();
+        data._mc4wp_honeypot = form.find('input[name="_mc4wp_honeypot"]').val();
+        data._mc4wp_timestamp = form.find('input[name="_mc4wp_timestamp"]').val();
+        data._mc4wp_form_element_id = form.find('input[name="_mc4wp_form_element_id"]').val();
+        
+        $.ajax({
+            type: 'POST',
+            data: data,
+            success: function(data){
+                console.log("Data: " + data);
+            }
+        });
+        return false;
+    });
 });
